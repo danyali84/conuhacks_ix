c11b3e2a3cbfb8a911202fdbffdd1a322146f0ad
danyali84 <lixdanya84@gmail.com>
2025-02-01 17:27:37 -0500

    eonnsnf

--- flightProject/interface/streamLit.py
+++ flightProject/interface/streamLit.py
@@ -1,9 +1,19 @@
 # Some interface code 
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
 
 # TODO Create a nav bar on the left side of the screen 
+home_Button = st.sidebar.button("Home")
+CP_Button = st.sidebar.button("C&P Button")
+UBar_Button = st.sidebar.button("UBar Button")
+#2 gates between cutsom z
+
+custom_Button1 = st.sidebar.text_input("Custom1", key="gate1", placeholder="Please enter a number")
 
 
-# TODO Create a place to display the data frame 
+# TODO Create a place to display the data frame s
 
 
 # TODO Create a section on the right to display some analytics boxes 
